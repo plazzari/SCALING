b4f2711c0651039dda96ceeceeca569e7433e9b7
Paolo Lazzari <plazzari@r000u07l02.marconi.cineca.it>
2019-04-26 13:34:45 +0200

    :: code cleaning

---
+++ sequence.sh
@@ -0,0 +1,8 @@
+module purge
+module load profile/advanced
+module load autoload 
+module load intel/pe-xe-2017--binary 
+module load python/2.7.12 scipy/0.18.1--python--2.7.12
+module load mpi4py/2.0.0--python--2.7.12
+
+source /marconi_work/OGS_dev_0/COPERNICUS/py_env_2.7.12/bin/activate
