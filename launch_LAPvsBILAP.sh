c12439349dee87069aaa00efb13c7c621e4bd099
Paolo Lazzari <plazzari@ogs.trieste.it>
2025-04-16 11:30:35 +0200

    Script to perform the experiment to compare the LAP vs BILAP for different wavenumbers

---
+++ launch_LAPvsBILAP.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+source /g100/home/userexternal/plazzari/sequence3.sh
+
+mkdir -p LAPvsBILAP_data LAPvsBILAP_plots
+
+#wn= wave number
+
+for wn in 2 5 10 20 30 40 50; do
+  python3 MODEL_1D_DET_ENS_FAST.py LAP ${wn} ; python3 MODEL_1D_DET_ENS_FAST.py BILAP ${wn} ; python plot_LAPvsBILAP.py ${wn}
+done
